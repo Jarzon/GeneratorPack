551c083c05faabdb3ced72be91fd22ce88fc9d79
Jarzon <jason@libellum.ca>
2026-01-27 16:16:13 -0500

    Set created/updated column default value to CURRENT_TIMESTAMP

--- assets/js/defaultLines.js
+++ assets/js/defaultLines.js
@@ -2,8 +2,8 @@ window.addEventListener('load', function () {
     if(lines.children.length === 0) {
         addLine('id', 'number', '', 'private', 1);
         addLine('status', 'number', '0', 'private', 1);
-        addLine('created', 'datetime', '', 'private', 1);
-        addLine('updated', 'datetime', '', 'private', 1);
+        addLine('created', 'datetime', 'CURRENT_TIMESTAMP', 'private', 1);
+        addLine('updated', 'datetime', 'CURRENT_TIMESTAMP', 'private', 1);
         addLine('user_id', 'number', '', 'private', 1);
     }
 });
